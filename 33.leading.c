9e725814771bcf2128825de7e78e61c6d9b2762e
akshaya1226 <124852751+akshaya1226@users.noreply.github.com>
2023-11-15 20:06:20 +0530

    Update 33.leading.c

--- 33.leading.c
+++ 33.leading.c
@@ -1,100 +1,43 @@
 #include<stdio.h>
 #include<ctype.h>
-void FIRST(char[],char );
-void addToResultSet(char[],char);
-int numOfProductions;
-char productionSet[10][10];
+void FIRST(char );
+int count,n=0;
+char prodn[10][10], first[10];
 main()
 {
-int i;
-char choice;
-char c;
-char result[20];
-printf("How many number of productions ? :");
-scanf(" %d",&numOfProductions);
-for(i=0;i<numOfProductions;i++)
-{
-printf("Enter productions Number %d : ",i+1);
-scanf(" %s",productionSet[i]);
-}
+int i,choice;
+char c,ch;
+printf("How many productions ? :");
+scanf("%d",&count);
+printf("Enter %d productions epsilon= $ :\n\n",count);
+for(i=0;i<count;i++)
+scanf("%s%c",prodn[i],&ch);
 do
 {
-printf("\n Find the FIRST of  :");
-scanf(" %c",&c);
-FIRST(result,c); 
+n=0;
+printf("Element :");
+scanf("%c",&c);
+FIRST(c);
 printf("\n FIRST(%c)= { ",c);
-for(i=0;result[i]!='\0';i++)
-printf(" %c ",result[i]);       
+for(i=0;i<n;i++)
+printf("%c ",first[i]);
 printf("}\n");
-printf("press 'y' to continue : ");
-scanf(" %c",&choice);
+printf("press 1 to continue : ");
+scanf("%d%c",&choice,&ch);
 }
-while(choice=='y'||choice =='Y');
+while(choice==1);
 }
-void FIRST(char* Result,char c)
+void FIRST(char c)
 {
-int i,j,k;
-char subResult[20];
-int foundEpsilon;
-subResult[0]='\0';
-Result[0]='\0';
-if(!(isupper(c)))
+int j;
+if(!(isupper(c)))first[n++]=c;
+for(j=0;j<count;j++)
 {
-addToResultSet(Result,c);
-return ;
+if(prodn[j][0]==c)
+{
+if(prodn[j][2]=='$') first[n++]='$';
+else if(islower(prodn[j][2]))first[n++]=prodn[j][2];
+else FIRST(prodn[j][2]);
 }
-for(i=0;i<numOfProductions;i++)
-{
-if(productionSet[i][0]==c)
-{
-if(productionSet[i][2]=='$') addToResultSet(Result,'$');
-else
-{
-j=2;
-while(productionSet[i][j]!='\0')
-{
-foundEpsilon=0;
-FIRST(subResult,productionSet[i][j]);
-for(k=0;subResult[k]!='\0';k++)
-addToResultSet(Result,subResult[k]);
-for(k=0;subResult[k]!='\0';k++)
-if(subResult[k]=='$')
-{
-foundEpsilon=1;
-break;
 }
-if(!foundEpsilon)
-
-                     break;
-
-                 j++;
-
-                }
-
-            }
-
-    }
-
-}
-
-    return ;
-
-}
-
-void addToResultSet(char Result[],char val)
-
-{
-
-    int k;
-
-    for(k=0 ;Result[k]!='\0';k++)
-
-        if(Result[k]==val)
-
-            return;
-
-    Result[k]=val;
-
-    Result[k+1]='\0';
-
 }
